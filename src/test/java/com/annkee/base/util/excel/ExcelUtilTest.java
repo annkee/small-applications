811e4ec450d29e52199a3d5d6cd3cbc2fd354191
wangan <wangan@supplyfintech.com>
2018-12-21 16:43:43 +0800

    excel

--- src/test/java/com/annkee/base/util/excel/ExcelUtilTest.java
+++ src/test/java/com/annkee/base/util/excel/ExcelUtilTest.java
@@ -1,17 +1,6 @@
 package com.annkee.base.util.excel;
 
-import org.apache.poi.hssf.usermodel.HSSFRow;
-import org.apache.poi.hssf.usermodel.HSSFSheet;
-import org.apache.poi.hssf.usermodel.HSSFWorkbook;
-import org.apache.poi.ss.usermodel.Workbook;
 import org.junit.Test;
-
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
-import java.io.FileOutputStream;
-
-import static org.junit.Assert.*;
 
 /**
  * @author wangan
